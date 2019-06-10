eb0dc843f95a2239c8eec1048e60a3e72b4f83f5
CN0072 <CN0072@CN0072.lan>
2019-06-10 12:12:37 +0100

    comments

--- bike.js
+++ bike.js
@@ -1,48 +1,60 @@
-function Bike(xy, color) {
-    this.x = xy[0];
-    this.y = xy[1];
-    this.xspeed = 1;
-    this.yspeed = 0;
-    this.total = 0;
-    this.tail = []
-    this.color = color
+//  Create bike class
+class Bike {
+    //  Taking position and color as parameters
+    constructor(xy, color) {
+        //  Initial position
+        this.x = xy[0];
+        this.y = xy[1];
+        //  Direction
+        this.xspeed = 1;
+        this.yspeed = 0;
+        //  Size
+        this.total = 0;
+        this.tail = []
 
+        this.color = color
+        this.lives = 3
 
-    this.death = () => {
-        for (i = 0; i < this.tail.length; i++) {
-            let pos = this.tail[i]
-            let d = dist(this.x, this.y, pos.x, pos.y)
-            if (d < 1) {
-                this.total = 0
-                this.tail = []
+        //  On self kill
+        this.death = () => {
+            for (i = 0; i < this.tail.length; i++) {
+                //  Checks if head position intersects tail
+                let pos = this.tail[i]
+                let d = dist(this.x, this.y, pos.x, pos.y)
+                if (d < 1) {
+                    //  Resets
+                    this.total = 0
+                    this.tail = []
+                }
             }
         }
-    }
 
-    this.update = () => {
-
-        this.total++
-        this.tail[this.total - 1] = createVector(this.x, this.y)
-
-
-
-        this.x = this.x + this.xspeed * scale
-        this.y = this.y + this.yspeed * scale
-        this.x = constrain(this.x, 0, width - scale)
-        this.y = constrain(this.y, 0, height - scale)
-    }
-
-    this.show = () => {
-        fill(this.color)
-        for (i = 0; i < this.total; i++) {
-            rect(this.tail[i].x, this.tail[i].y, scale, scale)
+        this.update = () => {
+            //  Increases in size per tick
+            this.total++
+            //  Draws tail in position
+            this.tail[this.total - 1] = createVector(this.x, this.y)
+            //  Moves depending on x and y
+            this.x = this.x + this.xspeed * scale
+            this.y = this.y + this.yspeed * scale
+            // Keeps bike in boundaries
+            this.x = constrain(this.x, 0, width - scale)
+            this.y = constrain(this.y, 0, height - scale)
         }
-        rect(this.x, this.y, scale, scale)
-    }
 
-    this.dir = (x, y) => {
-        this.xspeed = x
-        this.yspeed = y
-    }
+        this.show = () => {
+            // Creates rectangles of the given colour
+            fill(this.color)
+            for (i = 0; i < this.total; i++) {
+                rect(this.tail[i].x, this.tail[i].y, scale, scale)
+            }
+            rect(this.x, this.y, scale, scale)
+        }
 
+        this.dir = (x, y) => {
+            //  Sets heading depending on key press
+            this.xspeed = x
+            this.yspeed = y
+        }
+    }
 }
--- main.js
+++ main.js
@@ -1,80 +1,76 @@
 /// <reference path="./p5.global-mode.d.ts" />
+//  Set scale
 let scale = 10
+//  Set players
 let players = 2
+//  Bikes array
 let bikes = []
+//  Positions of bikes
 let position = [
     [20, 560],
     [20, 20]
 ]
+// Colours of bikes
 let color = ["#88C8DE", "#FBE955"]
 
 
 
 
-
+//  Initial setup
 function setup() {
+    //  Draw canvas
     createCanvas(600, 600)
+    //  Create bike objects depending on players
     for (i = 0; i < players; i++) {
         bikes[i] = new Bike(
             position[i],
             color[i]
-
         )
     }
-
-    // bike = new Bike(20, 560, "#FBE955")
-    // bike2 = new Bike(20, 20, "#88C8DE")
-    frameRate(10)
+    //  Set framerate
+    frameRate(15)
 }
 
 
 
-
+//  Drawing
 function draw() {
+    //  set background colour
     background(51)
-
-    // for (i = 0; i < players; i++) {
-    //     bikes[i].death()
-    //     bikes[i].update()
-    //     bikes[i].show()
-    // }
-
-    bikes[0].death()
-    bikes[0].update()
-    bikes[0].show()
-    bikes[1].death()
-    bikes[1].update()
-    bikes[1].show()
-
-
-    for (i = 0; i < bikes[1].tail.length; i++) {
-        let pos = bikes[1].tail[i]
-        let d = dist(bikes[0].x, bikes[0].y, pos.x, pos.y)
-        if (d < 1) {
-            bikes[0].total = 0
-            bikes[0].tail = []
+    // Call bike functions depending on players
+    if (players == 2) {
+        bikes[0].death()
+        bikes[0].update()
+        bikes[0].show()
+        bikes[1].death()
+        bikes[1].update()
+        bikes[1].show()
+        // Bike collisions with each other
+        //  Bike 1
+        for (i = 0; i < bikes[1].tail.length; i++) {
+            let pos = bikes[1].tail[i]
+            let d = dist(bikes[0].x, bikes[0].y, pos.x, pos.y)
+            if (d < 1) {
+                bikes[0].total = 0
+                bikes[0].tail = []
+            }
+        }
+        //  Bike 2
+        for (i = 0; i < bikes[0].tail.length; i++) {
+            let pos = bikes[0].tail[i]
+            let d = dist(bikes[1].x, bikes[1].y, pos.x, pos.y)
+            if (d < 1) {
+                bikes[1].total = 0
+                bikes[1].tail = []
+            }
         }
     }
-
-    for (i = 0; i < bikes[0].tail.length; i++) {
-        let pos = bikes[0].tail[i]
-        let d = dist(bikes[1].x, bikes[1].y, pos.x, pos.y)
-        if (d < 1) {
-            bikes[1].total = 0
-            bikes[1].tail = []
-        }
-    }
-
-    // bike.death()
-    // bike2.death()
-    // bike.update()
-    // bike2.update()
-    // bike.show()
-    // bike2.show()
 
 
 }
 
+
+//  Listens for pressed keys and changes direction of bike accordingly
 function keyPressed() {
     if (keyCode === UP_ARROW && bikes[0].yspeed != 1) {
         bikes[0].dir(0, -1)
