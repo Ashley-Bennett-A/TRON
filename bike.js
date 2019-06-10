83aa77dedebc1f0be781e3b19435b284e9c3b0a3
CN0072 <CN0072@CN0072.lan>
2019-06-10 10:53:24 +0100

    2 player colisions

--- bike.js
+++ bike.js
@@ -1,17 +1,43 @@
-function Bike() {
-    this.x = 0;
-    this.y = 0;
+function Bike(xy, color) {
+    this.x = xy[0];
+    this.y = xy[1];
     this.xspeed = 1;
     this.yspeed = 0;
+    this.total = 0;
+    this.tail = []
+    this.color = color
+
+
+    this.death = () => {
+        for (i = 0; i < this.tail.length; i++) {
+            let pos = this.tail[i]
+            let d = dist(this.x, this.y, pos.x, pos.y)
+            if (d < 1) {
+                this.total = 0
+                this.tail = []
+            }
+        }
+    }
 
     this.update = () => {
-        this.x = this.x + this.xspeed
-        this.y = this.y + this.yspeed
+
+        this.total++
+        this.tail[this.total - 1] = createVector(this.x, this.y)
+
+
+
+        this.x = this.x + this.xspeed * scale
+        this.y = this.y + this.yspeed * scale
+        this.x = constrain(this.x, 0, width - scale)
+        this.y = constrain(this.y, 0, height - scale)
     }
 
     this.show = () => {
-        fill(255)
-        rect(this.x, this.y, 10, 10)
+        fill(this.color)
+        for (i = 0; i < this.total; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scale, scale)
+        }
+        rect(this.x, this.y, scale, scale)
     }
 
     this.dir = (x, y) => {
--- main.js
+++ main.js
@@ -1,26 +1,96 @@
 /// <reference path="./p5.global-mode.d.ts" />
+let scale = 10
+let players = 2
+let bikes = []
+let position = [
+    [20, 560],
+    [20, 20]
+]
+let color = ["#88C8DE", "#FBE955"]
+
+
+
 
 
 function setup() {
     createCanvas(600, 600)
-    bike = new Bike()
+    for (i = 0; i < players; i++) {
+        bikes[i] = new Bike(
+            position[i],
+            color[i]
+
+        )
+    }
+
+    // bike = new Bike(20, 560, "#FBE955")
+    // bike2 = new Bike(20, 20, "#88C8DE")
+    frameRate(10)
 }
+
+
+
 
 function draw() {
     background(51)
 
-    bike.update()
-    bike.show()
+    // for (i = 0; i < players; i++) {
+    //     bikes[i].death()
+    //     bikes[i].update()
+    //     bikes[i].show()
+    // }
+
+    bikes[0].death()
+    bikes[0].update()
+    bikes[0].show()
+    bikes[1].death()
+    bikes[1].update()
+    bikes[1].show()
+
+
+    for (i = 0; i < bikes[1].tail.length; i++) {
+        let pos = bikes[1].tail[i]
+        let d = dist(bikes[0].x, bikes[0].y, pos.x, pos.y)
+        if (d < 1) {
+            bikes[0].total = 0
+            bikes[0].tail = []
+        }
+    }
+
+    for (i = 0; i < bikes[0].tail.length; i++) {
+        let pos = bikes[0].tail[i]
+        let d = dist(bikes[1].x, bikes[1].y, pos.x, pos.y)
+        if (d < 1) {
+            bikes[1].total = 0
+            bikes[1].tail = []
+        }
+    }
+
+    // bike.death()
+    // bike2.death()
+    // bike.update()
+    // bike2.update()
+    // bike.show()
+    // bike2.show()
+
+
 }
 
 function keyPressed() {
-    if (keyCode === UP_ARROW) {
-        bike.dir(0, -1)
-    } else if (keyCode === DOWN_ARROW) {
-        bike.dir(0, 1)
-    } else if (keyCode === LEFT_ARROW) {
-        bike.dir(-1, 0)
-    } else if (keyCode === RIGHT_ARROW) {
-        bike.dir(1, 0)
+    if (keyCode === UP_ARROW && bikes[0].yspeed != 1) {
+        bikes[0].dir(0, -1)
+    } else if (keyCode === DOWN_ARROW && bikes[0].yspeed != -1) {
+        bikes[0].dir(0, 1)
+    } else if (keyCode === LEFT_ARROW && bikes[0].xspeed != 1) {
+        bikes[0].dir(-1, 0)
+    } else if (keyCode === RIGHT_ARROW && bikes[0].xspeed != -1) {
+        bikes[0].dir(1, 0)
+    } else if (keyCode === 87 && bikes[1].yspeed != 1) {
+        bikes[01].dir(0, -1)
+    } else if (keyCode === 83 && bikes[1].yspeed != -1) {
+        bikes[1].dir(0, 1)
+    } else if (keyCode === 65 && bikes[1].xspeed != 1) {
+        bikes[1].dir(-1, 0)
+    } else if (keyCode === 68 && bikes[1].xspeed != -1) {
+        bikes[1].dir(1, 0)
     }
 }
